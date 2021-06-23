74c0ef0d1bcf017706532d8808659fa72e68fbcb
tanvir-ahmod <tanvirahmodcse@gmail.com>
2021-06-23 19:58:40 +0600

    Implement loader in login screen

--- lib/controller/auth_controller.dart
+++ lib/controller/auth_controller.dart
@@ -4,18 +4,16 @@ import 'package:flutterapp/data/repositories/auth/auth_repository_impl.dart';
 import 'package:get/get.dart';
 
 class AuthController extends GetxController {
-  var isLogin = false.obs;
+  var isLoading = false.obs;
   AuthRepository authRepository = AuthRepositoryImpl();
 
   Future<bool> login(String name, String password) async {
+    isLoading.value = true;
     var loginResponse = await authRepository
         .login(LoginRequest(name: name, password: password));
-
-    Get.snackbar(
-      "Authentication",
-      loginResponse.responseMessage,
-      snackPosition: SnackPosition.BOTTOM
-    );
+    isLoading.value = false;
+    Get.snackbar("Authentication", loginResponse.responseMessage,
+        snackPosition: SnackPosition.BOTTOM);
 
     return loginResponse.token.isNotEmpty;
   }
--- lib/ui/LoginScreen.dart
+++ lib/ui/LoginScreen.dart
@@ -14,105 +14,114 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text("Login Page"),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 40.0),
-                child: Center(
-                  child: Container(
-                      width: 200,
-                      height: 150,
-                      child: Image.asset('assets/images/password_setup.png')),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Form(
-                key: _form,
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                child: Column(
-                  children: [
-                    Padding(
-                      //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
-                      padding: EdgeInsets.symmetric(horizontal: 15),
-                      child: TextFormField(
-                        controller: _name,
-                        textInputAction: TextInputAction.next,
-                        validator: (val) {
-                          if (val != null && val.isEmpty)
-                            return 'Field can not be empty';
-                          return null;
-                        },
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Username',
-                            hintText: 'Enter username'),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          left: 15.0, right: 15.0, top: 15, bottom: 0),
-                      //padding: EdgeInsets.symmetric(horizontal: 15),
-                      child: TextFormField(
-                        controller: _pass,
-                        obscureText: true,
-                        textInputAction: TextInputAction.send,
-                        validator: (val) {
-                          if (val != null && val.isEmpty)
-                            return 'Field can not be empty';
-                          return null;
-                        },
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Password',
-                            hintText: 'Enter password'),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                height: 50,
-                width: 250,
-                decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(20)),
-                child: TextButton(
-                  onPressed: () async {
-                    if (_form.currentState!.validate()) {
-                      var isAuthenticated =
-                          await authController.login(_name.text, _pass.text);
-                      if (isAuthenticated) {
-                        Get.offAll(() => MyHomePage());
-                      }
-                    }
-                  },
-                  child: Text(
-                    'Login',
-                    style: TextStyle(color: Colors.white, fontSize: 25),
+    return Obx(() {
+      return Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            title: Text("Login Page"),
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 40.0),
+                  child: Center(
+                    child: Container(
+                        width: 200,
+                        height: 150,
+                        child: Image.asset('assets/images/password_setup.png')),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 130,
-              ),
-              InkWell(
-                  onTap: () {
-                    Get.offAll(SignUpUI());
-                  },
-                  child: Text('New User? Create Account'))
-            ],
-          ),
-        ));
+                SizedBox(
+                  height: 20,
+                ),
+                Form(
+                  key: _form,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  child: Column(
+                    children: [
+                      Padding(
+                        //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        child: TextFormField(
+                          controller: _name,
+                          textInputAction: TextInputAction.next,
+                          validator: (val) {
+                            if (val != null && val.isEmpty)
+                              return 'Field can not be empty';
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Username',
+                              hintText: 'Enter username'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 15.0, right: 15.0, top: 15, bottom: 0),
+                        //padding: EdgeInsets.symmetric(horizontal: 15),
+                        child: TextFormField(
+                          controller: _pass,
+                          obscureText: true,
+                          textInputAction: TextInputAction.send,
+                          validator: (val) {
+                            if (val != null && val.isEmpty)
+                              return 'Field can not be empty';
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Password',
+                              hintText: 'Enter password'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 50,
+                  width: 250,
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: TextButton(
+                    onPressed: () async {
+                      if (_form.currentState!.validate()) {
+                        var isAuthenticated =
+                            await authController.login(_name.text, _pass.text);
+                        if (isAuthenticated) {
+                          Get.offAll(() => MyHomePage());
+                        }
+                      }
+                    },
+                    child: Text(
+                      'Login',
+                      style: TextStyle(color: Colors.white, fontSize: 25),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                if (authController.isLoading.value)
+                  Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                SizedBox(
+                  height: 50,
+                ),
+                InkWell(
+                    onTap: () {
+                      Get.offAll(SignUpUI());
+                    },
+                    child: Text('New User? Create Account'))
+              ],
+            ),
+          ));
+    });
   }
 }
