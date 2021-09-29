6aa4d720a082e59ca9bbf3318e74d1770e8b86bd
tanvir-ahmod <tanvirahmodcse@gmail.com>
2021-09-29 18:22:02 +0600

    Update Constants.dart

--- lib/helpers/Constants.dart
+++ lib/helpers/Constants.dart
@@ -2,8 +2,8 @@ const TOKEN = "token";
 const PROJECT = "project";
 const PARENT_ID = "parentID";
 const UPDATE_LISTENER = "updateListener";
-// const BASE_URL = "https://sheltered-falls-24238.herokuapp.com/";
-const BASE_URL = "http://192.168.0.102:8080/";
+const BASE_URL = "https://sheltered-falls-24238.herokuapp.com/";
+// const BASE_URL = "http://192.168.0.102:8080/";
 
 const int RESPONSE_OK = 200;
 
